05b773a2ee3d41fab1c129ea3a9a50d19a95c3ce
Vivek <vivek@D-69-91-138-181.dhcp4.washington.edu>
2017-11-27 19:28:30 -0800

    rm data folder

---
+++ js/ScatterPlot.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class ScatterPlot extends React.PureComponent {
+    render() {
+
+        return (
+            <div className="joke">
+                ScatterPlot goes here.
+            </div >
+        );
+    }
+}
