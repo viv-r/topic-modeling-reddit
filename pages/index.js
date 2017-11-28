8f78edeb54888dc0e80034f017f8aa77fd94c224
Vivek <vivek@D-69-91-138-181.dhcp4.washington.edu>
2017-11-27 19:28:53 -0800

    rm data folder

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import data from '../data/reddit_jokes.json'
 import JokeList from '../js/JokeList'
+import DensityPlot from '../js/DensityPlot'
+import ScatterPlot from '../js/ScatterPlot'
 import Filter from '../js/Filter'
 
 export default class Main extends React.Component {
@@ -12,7 +14,6 @@ export default class Main extends React.Component {
         };
     }
     onFilterChange = filter => {
-        console.log("filter: ", filter)
         this.setState({
             filter,
         });
@@ -21,6 +22,32 @@ export default class Main extends React.Component {
     render() {
         return (
             <div>
+                <nav id="interactions">
+                    <select id="topic_a_selector" class="selector topic_selector">
+                        <option value="1" selected>Topic 1</option>
+                        <option value="2">Topic 2</option>
+                        <option value="3">Topic 3</option>
+                    </select>
+                    <select id="topic_b_selector" class="selector topic_selector">
+                        <option value="1">Topic 1</option>
+                        <option value="2" selected>Topic 2</option>
+                        <option value="3">Topic 3</option>
+                    </select>
+                    <button id="word_selector" class="selector word_joke_selector active" onclick="switch_word_joke_filter(0)">words</button>
+                    <button id="joke_selector" class="selector word_joke_selector" onclick="switch_word_joke_filter(1)">jokes</button>
+
+                    <select id="size_mapping" class="selector topic_selector">
+                        <option value="frequency">count</option>
+                        <option value="score">score</option>
+                        <option value="tfidf">tf/idf</option>
+                    </select>
+                </nav>
+                <div>
+                    <DensityPlot data={data} />
+                </div>
+                <div>
+                    <ScatterPlot data={data} />
+                </div>
                 <Filter filter={this.state.filter} onChange={this.onFilterChange} />
                 <JokeList filter={this.state.filter} data={this.state.data} />
             </div>
