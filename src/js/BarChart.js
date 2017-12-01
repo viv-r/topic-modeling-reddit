1cdfeeac905846dca72122f363b95d01c56a4e88
lukew <luke.waninger@gmail.com>
2017-11-30 21:22:30 -0800

    .

--- src/js/BarChart.js
+++ src/js/BarChart.js
@@ -17,53 +17,26 @@ const BarChart = Svg((node, props) => {
 
     const fill = (d, i) => i % 2 === 0 ? '#444' : '#333';
 
-<<<<<<< HEAD:js/BarChart.js
-    select(node)
-=======
-    d3.select(node)
->>>>>>> a240aef24be6091b197bbd4c0f66020968fc2831:src/js/BarChart.js
-        .attr('id', props.id)
-        .selectAll('rect')
-        .remove()
-
     d3.select(node)
         .selectAll('rect')
         .data(props.data)
         .enter()
         .append('rect')
-<<<<<<< HEAD:js/BarChart.js
             .attr('y', (d, i) => i * (props.barWidth + props.barSpacing))
             .attr('x', d => 0)
             .attr('width', d => yScale(d))
             .attr('height', props.barWidth)
             .style('fill', fill)
             .on('mouseover', (data, index, nodes) => {
-                select(nodes[index])
+                d3.select(nodes[index])
                     .style('fill', '#5555aa')
             })
             .on('mouseout', function (data, index, nodes) {
-                select(nodes[index])
+                d3.select(nodes[index])
                     .transition()
                     .duration(200)
                     .style('fill', fill)
             })
-=======
-        .attr('y', (d, i) => i * (props.barWidth + props.barSpacing))
-        .attr('x', d => 0)
-        .attr('width', d => yScale(d))
-        .attr('height', props.barWidth)
-        .style('fill', fill)
-        .on('mouseover', (data, index, nodes) => {
-            d3.select(nodes[index])
-                .style('fill', '#5555aa')
-        })
-        .on('mouseout', function (data, index, nodes) {
-            d3.select(nodes[index])
-                .transition()
-                .duration(200)
-                .style('fill', fill)
-        })
->>>>>>> a240aef24be6091b197bbd4c0f66020968fc2831:src/js/BarChart.js
 });
 
 export default BarChart;
