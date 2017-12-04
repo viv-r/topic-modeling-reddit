a8b9c8ddff20d5c300723a59471b363448da3a91
LukeW <luke.waninger@gmail.com>
2017-12-04 15:00:45 -0800

    azure

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 var http = require('http');
 
 http.createServer(function (req, res) {
+
     fs.readFile("./public/index.html" + request.url,function(error,data){
+        response.writeHead(404,{"Content-type":"text/plain"});
+        response.end("Soryolo found");
+
         if(error){
             response.writeHead(404,{"Content-type":"text/plain"});
             response.end("Sorry the page was not found");
