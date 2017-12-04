b6c16f1da71e8a88cb5d9199a8d94b87ecdc44c5
LukeW <luke.waninger@gmail.com>
2017-12-04 14:37:39 -0800

    test azure deployment

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 var http = require('http');
 
 http.createServer(function (req, res) {
-    
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hello, world!');
-    
+    fs.readFile("./public/index.html" + request.url,function(error,data){
+        if(error){
+            response.writeHead(404,{"Content-type":"text/plain"});
+            response.end("Sorry the page was not found");
+        }else{
+            response.writeHead(202,{"Content-type":"text/html"});
+            response.end(data);
+        }
+    }); 
 }).listen(process.env.PORT || 8080);
