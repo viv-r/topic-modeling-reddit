e5fb8730357efaa908c30c84da2d4f365180e891
lukeWaninger <luke.waninger@gmail.com>
2017-12-11 12:51:55 -0800

    .

--- src/js/ScatterPlot.js
+++ src/js/ScatterPlot.js
@@ -206,8 +206,8 @@ const Graph = Svg((node, props) => {
             d3.select("#tooltip")
                 .html(
                 "<h4>" + d.name + "</h4>" +
-                "<p><em>occurrences in topic " + props.ta_num + ":</em>" + d.p_topicA +
-                "</br><em>occurrences of topic " + props.tb_num + ":</em>" + d.p_topicB +
+                "<p><em>occurrences in topic " + (props.ta_num+1) + ":</em>" + d.p_topicA +
+                "</br><em>occurrences of topic " + (props.tb_num+1) + ":</em>" + d.p_topicB +
                 "</br><em>occurrences in all topics:</em> " + d.count +
                 "</p>"
                 )
